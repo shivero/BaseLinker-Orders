267e6fc0bf99e02999c69412f419341b02d6c637
Grzegorz Dziubek <grzesiekdziubek@gmail.com>
2021-07-08 10:21:24 +0200

    Add project files.

---
+++ BaseLinker-Orders/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLinker_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
